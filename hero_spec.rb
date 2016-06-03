58ad71374f2c2859504de1ef6dcf281b8c3580b4
alceo <alceo@alceo.ru>
2016-06-03 11:39:30 +0300

    refactor test + power down

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,21 +1,24 @@
 require './hero.rb'
 
 describe Hero do
-
+before do
+@hero = Hero.new 'mike'
+end 
 it "has a capitalize name" do
-
-  hero = Hero.new 'mike'
-
-  expect(hero.name).to eq 'Mike' #hero.name = 'Mike'
-
+  expect(@hero.name).to eq 'Mike' #hero.name = 'Mike'
 end
 
 it "can power up " do
+  expect(@hero.power_up).to eq 110
+end
 
-  hero = Hero.new 'mike'
-  expect(hero.power_up).to eq 110
 
-  end
+it "can power down " do
+ expect(@hero.power_down).to eq 90
+end
 
+it "can show info" do
+  expect(@hero.hero_info).to eq 'Mike has health 100'
+end
   
 end
